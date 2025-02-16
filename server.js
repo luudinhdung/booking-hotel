4176921aa92b92c84df4c9d5e67ac28f48857707
DESKTOP-1VIGCT6\Admin <nguyendinhtuananh7@gmail.com>
2025-02-16 21:21:19 +0700

    Cập nhật logic đăng ký

--- server.js
+++ server.js
@@ -1,14 +1,57 @@
-require('dotenv').config();
-const app = require('./src/app')
-
-const {PORT} = process.env;
-
-
-
-const server = app.listen( PORT, () => {
-    console.log(`WSV start with port ${PORT}`);
-})
-
-process.on('SIGINT', () => {
-    server.close( () => console.log(`exits server express`))
-})
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 6060
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const jwtSecret = 'aunasdfsdffsdjlksfdjl'
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const db= require('./config/index')
+const multer=require('multer')
+const upload = multer({ dest: 'uploads/' })
+const path = require('path')
+const fs = require('fs')
+const UserModel = require('./models/User')
+const PlaceModel = require('./models/Places')
+const BookingModel = require('./models/Booking')
+const CommentModel = require('./models/Comment')
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+db.connect()
+app.use(cors({
+  credentials: true,
+  origin: true 
+ }));
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+extended:true
+}))
+//xQYLaGDmO7JLesCq
+app.post('/register',async (req,res)=>{
+    const {name,email,password,role,avatar} = req.body
+    try {
+       const checkEmail = await UserModel.findOne({
+         email:email
+       })
+       if(checkEmail){
+         return res.json({
+           mess:'tai khoan da ton tai'
+         })
+       }else if(!checkEmail){
+          await UserModel.create({
+            name,
+             email,
+             password:bcrypt.hashSync(password,salt),
+             role,
+             avatar
+         })
+         return res.json({
+           mess:'dang ki thanh cong'
+         })
+       }
+    } catch (error) {
+        res.status(422).json(error)
+    }
+   })
