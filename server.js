13be949b292f6083579e8c511ae29e3a05410897
Dung <luudinhdung2402@gmail.com>
2025-02-24 10:53:28 +0700

    update

--- server.js
+++ server.js
@@ -3,8 +3,30 @@ const app = require('./src/app')
 
 const {PORT} = process.env;
 
-
-
+app.post('/places',async (req,res)=>{
+    const {title,address,addedPhotos,desc,checkIn,checkOut,maxGuests,price}= req.body
+    const {token} = req.cookies
+    if(token){
+      jwt.verify(token,jwtSecret,async (err,userData)=>{
+        if(err)throw err
+        const placeDoc = await PlaceModel.create({
+          title,address,addedPhotos,desc,checkIn,checkOut,maxGuests,price
+         })
+         return res.json(placeDoc)
+      })
+    }
+  
+  })
+  
+  app.get('/place',async (req,res)=>{
+     const data = await PlaceModel.find({})
+     res.json(data)
+  })
+  app.get('/places/:id',async (req,res)=>{
+    const {id}= req.params
+    const data = await PlaceModel.findById(id)
+    res.json(data)
+  })
 const server = app.listen( PORT, () => {
     console.log(`WSV start with port ${PORT}`);
 })
